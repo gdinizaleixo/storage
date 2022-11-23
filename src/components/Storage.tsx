d26784a620219970ca7b8f6d032876e9bfceccdc
Sdinizz <gdiniz0011@gmail.com>
2022-11-23 03:20:48 -0300

    fix storage table currency

--- src/components/Storage.tsx
+++ src/components/Storage.tsx
@@ -26,6 +26,11 @@ export default function Storage() {
   const [isOpenUpdate, setIsOpenUpdate] = useState(false);
   const [itemId, setItemId] = useState<number | undefined>();
 
+  var formatter = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+
   const columnHelper = createColumnHelper<any>();
 
   const columns = [
@@ -35,7 +40,7 @@ export default function Storage() {
     }),
     columnHelper.accessor("product_price", {
       header: "Preço",
-      cell: (info) => info.getValue(),
+      cell: (info) => formatter.format(info.getValue()),
     }),
     columnHelper.accessor("product_quantity", {
       header: () => "Quantidade",
@@ -83,17 +88,11 @@ export default function Storage() {
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
-                  if (
-                    header.id === "product_edit" ||
-                    header.id === "product_delete"
-                  ) {
+                  if (header.id === "product_edit" || header.id === "product_delete") {
                     return (
                       <th key={header.id} colSpan={header.colSpan}>
                         <div className=" select-none px-5">
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                          {flexRender(header.column.columnDef.header, header.getContext())}
                         </div>
                       </th>
                     );
@@ -105,17 +104,10 @@ export default function Storage() {
                           className="cursor-pointer select-none px-5"
                           onClick={header.column.getToggleSortingHandler()}
                         >
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                          {flexRender(header.column.columnDef.header, header.getContext())}
                           {{
-                            asc: (
-                              <ArrowUp className="w-4 h-4 ml-2 inline-block" />
-                            ),
-                            desc: (
-                              <ArrowDown className="w-4 h-4 ml-2 inline-block" />
-                            ),
+                            asc: <ArrowUp className="w-4 h-4 ml-2 inline-block" />,
+                            desc: <ArrowDown className="w-4 h-4 ml-2 inline-block" />,
                           }[header.column.getIsSorted() as string] ?? null}
                         </button>
                       </th>
@@ -159,10 +151,7 @@ export default function Storage() {
                   } else
                     return (
                       <td key={cell.id} className="text-center">
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
                       </td>
                     );
                 })}
@@ -174,10 +163,7 @@ export default function Storage() {
               <tr key={footerGroup.id}>
                 {footerGroup.headers.map((header) => (
                   <th key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.footer,
-                      header.getContext()
-                    )}
+                    {flexRender(header.column.columnDef.footer, header.getContext())}
                   </th>
                 ))}
               </tr>
@@ -218,10 +204,7 @@ export default function Storage() {
   });
 
   async function selectProduct() {
-    const { data } = await supabase
-      .from<Product>("product")
-      .select()
-      .throwOnError();
+    const { data } = await supabase.from<Product>("product").select().throwOnError();
     return data;
   }
 
@@ -257,9 +240,7 @@ export default function Storage() {
     };
 
     try {
-      const { data, error } = await supabase
-        .from("product")
-        .insert([newProduct]);
+      const { data, error } = await supabase.from("product").insert([newProduct]);
       productNameRef.current.value = "";
       productPriceRef.current.value = "";
       productQuantityRef.current.value = "";
@@ -278,10 +259,7 @@ export default function Storage() {
       product_price: productPriceRef.current?.value,
     };
 
-    const { data, error } = await supabase
-      .from("product")
-      .update(modified)
-      .match({ product_id });
+    const { data, error } = await supabase.from("product").update(modified).match({ product_id });
     if (error) {
       return toast.error("Preencha algo valído");
     }
@@ -291,10 +269,7 @@ export default function Storage() {
   }
 
   async function deleteProduct(product_id: number | undefined) {
-    const { data, error } = await supabase
-      .from("product")
-      .delete()
-      .match({ product_id });
+    const { data, error } = await supabase.from("product").delete().match({ product_id });
     handleChange();
   }
 
@@ -308,9 +283,7 @@ export default function Storage() {
     <main className="mt-10">
       <div className="flex justify-center text-white">
         <section className="flex flex-col gap-10">
-          <h1 className="text-5xl mt-20 decoration-double font-medium text-center ">
-            Estoque
-          </h1>
+          <h1 className="text-5xl mt-20 decoration-double font-medium text-center ">Estoque</h1>
           <TabelaEstoque />
           <div>
             <div className="flex items-center justify-center">
@@ -360,11 +333,7 @@ export default function Storage() {
                             className="flex justify-center flex-col gap-3"
                           >
                             <label>Nome:</label>
-                            <input
-                              className="btn_class"
-                              type="text"
-                              ref={productNameRef}
-                            />
+                            <input className="btn_class" type="text" ref={productNameRef} />
                             <label>Preço:</label>
                             <input
                               className="btn_class"
@@ -390,12 +359,7 @@ export default function Storage() {
                           onClick={closeModal}
                           className="absolute top-1 right-1 hover:bg-red-400 rounded-xl flex items-center"
                         >
-                          <Image
-                            width={24}
-                            height={24}
-                            src="/Close.svg"
-                            alt="Close modal"
-                          />
+                          <Image width={24} height={24} src="/Close.svg" alt="Close modal" />
                         </button>
                       </Dialog.Panel>
                     </Transition.Child>
@@ -407,11 +371,7 @@ export default function Storage() {
             {/* SEPARA MODAISSSSSSSSSSSSSSSSSSSSS */}
 
             <Transition appear show={isOpenDelete} as={Fragment}>
-              <Dialog
-                as="div"
-                className="relative z-10"
-                onClose={closeModalDelete}
-              >
+              <Dialog as="div" className="relative z-10" onClose={closeModalDelete}>
                 <Transition.Child
                   as={Fragment}
                   enter="ease-out duration-300"
@@ -436,10 +396,7 @@ export default function Storage() {
                       leaveTo="opacity-0 scale-95"
                     >
                       <Dialog.Panel className="w-[700px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                        <Dialog.Title
-                          as="h3"
-                          className=" text-center font-bold text-[20px] "
-                        >
+                        <Dialog.Title as="h3" className=" text-center font-bold text-[20px] ">
                           Realmente deseja excluir?
                         </Dialog.Title>
                         <div className="mt-4 flex justify-center items-center font-bold gap-3">
@@ -465,12 +422,7 @@ export default function Storage() {
                           onClick={closeModalDelete}
                           className="absolute top-1 right-1 hover:bg-red-400 rounded-xl flex items-center"
                         >
-                          <Image
-                            width={24}
-                            height={24}
-                            src="/Close.svg"
-                            alt="Close modal"
-                          />
+                          <Image width={24} height={24} src="/Close.svg" alt="Close modal" />
                         </button>
                       </Dialog.Panel>
                     </Transition.Child>
@@ -482,11 +434,7 @@ export default function Storage() {
             {/* SEPARA MODAISSSSSSSSSSSSSSSSSSSSS */}
 
             <Transition appear show={isOpenUpdate} as={Fragment}>
-              <Dialog
-                as="div"
-                className="relative z-10"
-                onClose={closeModalUpdate}
-              >
+              <Dialog as="div" className="relative z-10" onClose={closeModalUpdate}>
                 <Transition.Child
                   as={Fragment}
                   enter="ease-out duration-300"
@@ -511,20 +459,13 @@ export default function Storage() {
                       leaveTo="opacity-0 scale-95"
                     >
                       <Dialog.Panel className="w-[700px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                        <Dialog.Title
-                          as="h3"
-                          className=" text-center font-bold text-[20px] "
-                        >
+                        <Dialog.Title as="h3" className=" text-center font-bold text-[20px] ">
                           Editar dados
                         </Dialog.Title>
                         <div className="mt-4 flex justify-center items-center font-bold">
                           <div className="flex justify-center flex-col gap-3">
                             <label>Nome:</label>
-                            <input
-                              className="btn_class"
-                              type="text"
-                              ref={productNameRef}
-                            />
+                            <input className="btn_class" type="text" ref={productNameRef} />
                             <label>Preço:</label>
                             <input
                               className="btn_class"
@@ -540,10 +481,7 @@ export default function Storage() {
                               min="1"
                               ref={productQuantityRef}
                             />
-                            <button
-                              className="btn"
-                              onClick={() => updateProduct(itemId)}
-                            >
+                            <button className="btn" onClick={() => updateProduct(itemId)}>
                               Editar
                             </button>
                           </div>
@@ -553,12 +491,7 @@ export default function Storage() {
                           onClick={closeModalUpdate}
                           className="absolute top-1 right-1 hover:bg-red-400 rounded-xl flex items-center"
                         >
-                          <Image
-                            width={24}
-                            height={24}
-                            src="/Close.svg"
-                            alt="Close modal"
-                          />
+                          <Image width={24} height={24} src="/Close.svg" alt="Close modal" />
                         </button>
                       </Dialog.Panel>
                     </Transition.Child>
